fdeb6896b332db16c11a950cf3cc1c6109f6cb3e
Ruslan Zubtsov <zubtsov.r@gmail.com>
2024-09-20 21:23:29 +0400

    - added implementation for 2 level nested string dictionary merge

--- src/pyrametrize/inheritance.py
+++ src/pyrametrize/inheritance.py
@@ -1,5 +1,32 @@
 from typing import Dict
 
 
+class ConfigurationMergeException(Exception):
+    pass
+
+
 def merge_configurations(parent_config: Dict, child_config: Dict) -> Dict:
-    parent_config.update(child_config)
+    merge_result = {}
+
+    child_only_keys = child_config.keys() - parent_config.keys()
+    for key in child_only_keys:
+        merge_result[key] = child_config[key]
+
+    parent_only_keys = parent_config.keys() - child_config.keys()
+    for key in parent_only_keys:
+        merge_result[key] = parent_config[key]
+
+    common_keys = parent_config.keys() & child_config.keys()
+    for key in common_keys:
+        pck = parent_config[key]
+        cck = child_config[key]
+        if isinstance(pck, str) and isinstance(cck, str):
+            merge_result[key] = (
+                cck  # TODO: here can be a customization of merge behavior
+            )
+        elif isinstance(pck, dict) and isinstance(cck, dict):
+            merge_result[key] = merge_configurations(pck, cck)
+        else:
+            raise ConfigurationMergeException("Can't merge ", pck, cck)
+
+    return merge_result
--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -2,12 +2,36 @@ from src.pyrametrize.inheritance import merge_configurations
 
 
 def test_1_level_string_dictionary_inheritance_merge():
-    modifiable_parent_config = {"field1": "parent_val1", "field2": "parent_val2"}
+    parent_config = {"field1": "parent_val1", "field2": "parent_val2"}
 
     child_config = {"field2": "child_val2", "field3": "child_val3"}
 
     expected = {"field1": "parent_val1", "field2": "child_val2", "field3": "child_val3"}
 
-    merge_configurations(modifiable_parent_config, child_config)
+    actual = merge_configurations(parent_config, child_config)
 
-    assert modifiable_parent_config == expected
+    assert actual == expected
+
+
+def test_2_level_string_dictionary_inheritance_merge():
+    parent_config = {
+        "field1": {
+            "field2": "parent_val2",
+            "field3": "parent_val3",
+            "field4": "parent_val4",
+        }
+    }
+
+    child_config = {"field1": {"field3": "child_val3", "field4": "child_val4"}}
+
+    expected = {
+        "field1": {
+            "field2": "parent_val2",
+            "field3": "child_val3",
+            "field4": "child_val4",
+        }
+    }
+
+    actual = merge_configurations(parent_config, child_config)
+
+    assert actual == expected
