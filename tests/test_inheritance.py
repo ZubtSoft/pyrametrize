ff14a49e9fac3a48a296f8b06f5febc45772fdd0
Ruslan Zubtsov <zubtsov.r@gmail.com>
2024-09-21 22:25:32 +0400

    - combined 2 tests

--- tests/test_inheritance.py
+++ tests/test_inheritance.py
@@ -1,35 +1,35 @@
 from src.pyrametrize.inheritance import merge_configurations
 
 
-def test_1_level_string_dictionary_inheritance_merge():
-    parent_config = {"field1": "parent_val1", "field2": "parent_val2"}
-
-    child_config = {"field2": "child_val2", "field3": "child_val3"}
-
-    expected = {"field1": "parent_val1", "field2": "child_val2", "field3": "child_val3"}
-
-    actual = merge_configurations(parent_config, child_config)
-
-    assert actual == expected
-
-
 def test_2_level_string_dictionary_inheritance_merge():
     parent_config = {
-        "field1": {
-            "field2": "parent_val2",
-            "field3": "parent_val3",
-            "field4": "parent_val4",
-        }
+        "field1": "parent_val1",
+        "field2": "child_val2",
+        "field4": {
+            "field5": "parent_val5",
+            "field6": "child_val6",
+            "field7": "child_val7",
+        },
     }
 
-    child_config = {"field1": {"field3": "child_val3", "field4": "child_val4"}}
+    child_config = {
+        "field2": "child_val2",
+        "field3": "child_val3",
+        "field4": {
+            "field6": "child_val6",
+            "field7": "child_val7",
+        },
+    }
 
     expected = {
-        "field1": {
-            "field2": "parent_val2",
-            "field3": "child_val3",
-            "field4": "child_val4",
-        }
+        "field1": "parent_val1",
+        "field2": "child_val2",
+        "field3": "child_val3",
+        "field4": {
+            "field5": "parent_val5",
+            "field6": "child_val6",
+            "field7": "child_val7",
+        },
     }
 
     actual = merge_configurations(parent_config, child_config)
